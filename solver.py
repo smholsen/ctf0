9f1ad7759a9b78c4cdba51d723316d559cc5237f
Simon Olsen <smholsen@stud.ntnu.no>
2017-09-19 13:45:11 +0200

    fixed the error by not reading ciphertext from file

--- solver.py
+++ solver.py
@@ -17,7 +17,7 @@ def modinv(a, m):
     g, x, y = egcd(a, m)
     if g != 1:
         raise Exception('No modular inverse')
-    return x%m
+    return x % m
 
 f = open('AlicePublickey.pem', 'r')
 AlicePublicKey = RSA.importKey(f.read())
@@ -25,9 +25,10 @@ f.close()
 f = open('BobPublickey.pem', 'r')
 BobPublicKey = RSA.importKey(f.read())
 f.close()
-f = open('Ciphertext.txt', 'r')
-cipherText = f.read()
-f.close()
+
+# f = open('Ciphertext.txt', 'r')
+# cipherText = f.read()
+# f.close()
 
 p = gcd(AlicePublicKey.n, BobPublicKey.n)
 print 'P', p
@@ -38,5 +39,6 @@ print 'lambdaN', lambdaN
 d = modinv(AlicePublicKey.e, lambdaN)
 print 'D', d
 
+cipherText = '@\n\xea\xa9\xd9C6\xfd]{:\xaf\xe0\xf6\xe3\x15\xb7\x92i\x0e}\x7f\x8c\xc5\xa9q\xcb\x94\xac{\xad\x0bU\xf5\x8b\xfe\xfbd\xa2\x8e\x19\xa0\xddN\xbb\xfc\xb3W\xcez\xa3\x17\xa8U\n\xf4\xda\xc4\xe1\xa1x"Gw\x00\x12\xd9`;\n\x0c\x94\xef\xe8S\xcd\xa8\x91\xb6\xf08\xeb\n\x8d\xa9u\xec]\xd8\xeb\x98\xf7\x9c\xdbW\xe1h}h\x92\x0f\xa0\x8d\xd8\x0c\x978\xe6\xf7\xc5\xdb\xaa\xca\xf1P\xbf\x1e5\xba\x9f\rc\x92CB\x1eX\x84'
 alicesPrivateKey = RSA.construct((AlicePublicKey.n, AlicePublicKey.e, d, p, q, None))
 print alicesPrivateKey.decrypt(cipherText)
